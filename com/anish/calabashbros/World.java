919d703e2b2dbf778f2ae4dcbf93239965f1eb38
DDDDDebugger <2542071198@qq.com>
2021-10-17 20:43:35 +0800

    task1

--- com/anish/calabashbros/World.java
+++ com/anish/calabashbros/World.java
@@ -1,11 +1,25 @@
 package com.anish.calabashbros;
+import java.awt.Color;
+import java.util.Random;
 
 public class World {
 
     public static final int WIDTH = 40;
     public static final int HEIGHT = 20;
+    private Calabash[] bros;
 
     private Tile<Thing>[][] tiles;
+
+    private class Matrix {
+        final int row = 8;
+        final int column = 8;
+
+        // Matrix(int row, int column){
+        //     this.column = column;
+        //     this.row = row;
+        // }
+    }
+    private Matrix matrix;
 
     public World() {
 
@@ -19,6 +33,12 @@ public class World {
                 tiles[i][j].setThing(new Floor(this));
             }
         }
+
+        matrix = new Matrix();
+        initBros();
+        putBros();
+        randomBros();
+        
     }
 
     public Thing get(int x, int y) {
@@ -27,6 +47,45 @@ public class World {
 
     public void put(Thing t, int x, int y) {
         this.tiles[x][y].setThing(t);
+    }
+
+    public Calabash[] getBros(){
+        return bros;
+    }
+
+    private void putBros(){
+        if(bros == null) return;
+        int startx = (WIDTH - matrix.column)/2;
+        int starty = (HEIGHT - matrix.row)/2;
+        for(int i = 0; i<bros.length; ++i){
+            this.put(bros[i],startx + i%matrix.column,starty + i/matrix.column);
+        }
+    }
+
+    private void initBros(){
+        if(bros != null) return;
+        int nums = matrix.row*matrix.column;
+        bros = new Calabash[nums];
+        for(int i = 0; i<nums; ++i){
+            bros[i] = new Calabash(new Color(255, i*4, 0), i, this);
+        }
+    }
+
+    private void randomBros(){
+        if(bros == null) return;
+        int matrixsize = matrix.row*matrix.column;
+        int changetimes = matrixsize/2;
+        Random random = new Random();
+        for(int i = 0; i < changetimes; ++i){
+            int i1 = random.nextInt(matrixsize);
+            int i2 = random.nextInt(matrixsize);
+            if(i1!=i2){
+                bros[i1].swap(bros[i2]);
+                Calabash temp = bros[i1];
+                bros[i1] = bros[i2];
+                bros[i2] = temp;
+            }
+        }
     }
 
 }
--- com/anish/screen/WorldScreen.java
+++ com/anish/screen/WorldScreen.java
@@ -18,23 +18,7 @@ public class WorldScreen implements Screen {
     public WorldScreen() {
         world = new World();
 
-        bros = new Calabash[7];
-
-        bros[3] = new Calabash(new Color(204, 0, 0), 1, world);
-        bros[5] = new Calabash(new Color(255, 165, 0), 2, world);
-        bros[1] = new Calabash(new Color(252, 233, 79), 3, world);
-        bros[0] = new Calabash(new Color(78, 154, 6), 4, world);
-        bros[4] = new Calabash(new Color(50, 175, 255), 5, world);
-        bros[6] = new Calabash(new Color(114, 159, 207), 6, world);
-        bros[2] = new Calabash(new Color(173, 127, 168), 7, world);
-
-        world.put(bros[0], 10, 10);
-        world.put(bros[1], 12, 10);
-        world.put(bros[2], 14, 10);
-        world.put(bros[3], 16, 10);
-        world.put(bros[4], 18, 10);
-        world.put(bros[5], 20, 10);
-        world.put(bros[6], 22, 10);
+        bros = world.getBros();
 
         BubbleSorter<Calabash> b = new BubbleSorter<>();
         b.load(bros);
